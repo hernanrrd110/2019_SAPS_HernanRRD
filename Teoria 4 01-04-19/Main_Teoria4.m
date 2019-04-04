5158b659a41ec4ab7985603014e397f9e1fa736d
hernyyc <43017231+hernyyc@users.noreply.github.com>
2019-04-04 19:12:25 -0300

    Commit 04-04-19
    Incluimos la relación señal ruido y el filtro para la senoidal

--- Teoria 4 01-04-19/Main_Teoria4.m
+++ Teoria 4 01-04-19/Main_Teoria4.m
@@ -11,7 +11,7 @@ close all;
 fprintf('.....Información.....\r\n');
 fprintf('.....Autor:Hernan_RRD.....\r\n');
 fprintf('.....Procesamiento de seniales de audio.....\r\n');
-%%%%% LECTURA DE LA SEÑAL DE AUDIO %%%%%
+%...................... LECTURA DE LA SEÑAL DE AUDIO ......................
 % Cuadro de diálogo para abrir archivos, guarda nombre de archivo y dirección
 [filename,pathname,~] = uigetfile('*.wav','Archivo de audio');
  
@@ -26,6 +26,8 @@ vector_senial_monoL  = vector_senial_estereo(:,1);
 
 
 %%
+%..................... ACONDICIONAMIENTO DE LA SENIAL .....................
+
 num_muestras = length(vector_senial_monoL);
 
 vector_tiempo = 0:1/frec_muestreo:(num_muestras-1)/frec_muestreo;
@@ -34,51 +36,70 @@ vector_tiempo = 0:1/frec_muestreo:(num_muestras-1)/frec_muestreo;
 %%%%% Extraemos el valor medio de la senial %%%%%
 vector_senial_monoL = vector_senial_monoL - mean(vector_senial_monoL);
 
-%%%%% CÁLCULO DE FFT Audio %%%%%
+%......................... CALCULO DE FFT AUDIO ..........................
+
 FFT1  = fft(vector_senial_monoL, num_muestras) / num_muestras;
-Modulo1 = 2*abs(FFT1);                                     % Módulo de FFT
+% Módulo de FFT
+Modulo1 = 2*abs(FFT1);
+
 %%%%Nos quedamos con las frecuencias positivas
 Modulo1 = Modulo1(1:floor(num_muestras/2));
 freq = frec_muestreo/2*linspace(0,1,floor(num_muestras/2));% vector frecuencias [Hz]
 
+%%%%% Calculo de Max %%%%%
 
-%%%%% Cálculo de Max %%%%%
-
-% M = max(Modulo1);
+M = max(Modulo1);
 % M = max(abs(vector_senial_monoL));
-M = 1;
+% M = 1;
 
-%%%%%SENIAL DE TONO PURO
+%..................... CALCULO DE TONO (FFT y SENIAL) .....................
+
 f_7k = 7000;
 seno7K = M*sin(2*pi*f_7k*vector_tiempo);
 
 seno7K = seno7K';
 
-%%%%% CÁLCULO DE FFT con 7K %%%%%
+vector_senial_sucia = vector_senial_monoL + seno7K;
+
+%%%%% CALCULO DE FFT con 7K %%%%%
 
 FFT1_con7K  = fft((vector_senial_monoL + seno7K), num_muestras) / num_muestras;
 Modulo_con7K = 2*abs(FFT1_con7K);
 Modulo_con7K = Modulo_con7K(1:floor(num_muestras/2)); %%%%floor = truncar
 
+%................ CALCULO DE RELACION SENIAL RUIDO........................
 
+Relacion_senial_ruido = sum((vector_senial_monoL).^2)/sum((seno7K).^2);
 
+Relacion_senial_ruido_matlab = snr(vector_senial_monoL,seno7K);
+Relacion_senial_ruido_matlab = 10^(Relacion_senial_ruido_matlab/20);
+
+%.............................. FILTRADO...................................
+
+load('Filtro_PB.mat')
+vector_senial_sucia_filtrada = filter(Coeficientes_flitroPB,1,vector_senial_sucia);
+
+%......................... FTT DEL SENIALFILTRADA .........................
+
+FFT_filtrada  = fft((vector_senial_sucia_filtrada), num_muestras) / num_muestras;
+Modulo_filtrada = 2*abs(FFT_filtrada);
+Modulo_filtrada = Modulo_filtrada(1:floor(num_muestras/2)); %%%%floor = truncar
 
 %%
-%%%%%%%%%%%%%%%% GRAFICAS %%%%%%%%%%%%%%%%%%
+%.............................. GRAFICAS ...............................
+
 close all;
 
 figure1 = figure ('Color',[1 1 1],'Name','Track Audio','NumberTitle','off');
 plot(vector_tiempo, vector_senial_monoL,'LineWidth',1);grid on;    
-                            
-
 
 title('Sinusoidal','FontSize',11,'FontName','Arial')
 xlabel('Tiempo [seg]','FontSize',11,'FontName','Arial')
 ylabel('Amplitud','FontSize',11,'FontName','Arial')
 
-%%%%% Graficación de Módulo de FFT %%%%%
+%................. GRAFICACION FFT senial sin ruido .......................
 figure2 = figure('Color',[1 1 1],'Name','Track Audio','NumberTitle','off');
-stem(freq, Modulo1, 'r','LineWidth',3);grid on;
+stem(freq, Modulo1, 'b','LineWidth',1);grid on;
 
 title('Espectro')
 xlabel('Frecuencia [Hz]','FontSize',11,'FontName','Arial')
@@ -86,21 +107,41 @@ ylabel('Modulo1','FontSize',11,'FontName','Arial')
 
 %sound(vector_senial_monoL,frec_muesteo);
           
-%%%%% Graficación de Módulo de FFT con 7K %%%%%
+%%..................... GRAFICACION FFT Tono 7K ...........................
 figure3 = figure('Color',[1 1 1],'Name','Track Audio con Tono 7K','NumberTitle','off');
-stem(freq, Modulo_con7K, 'r','LineWidth',3);grid on;
+stem(freq, Modulo_con7K, 'r','LineWidth',1);grid on;
 
 title('Espectro')
 xlabel('Frecuencia [Hz]','FontSize',11,'FontName','Arial')
 ylabel('Modulo1','FontSize',11,'FontName','Arial')
 
-%%%%% Graficación de la senial de FFT %%%%%
+%................. GRAFICACION senial con ruido .......................
+
 figure4 = figure('Color',[1 1 1],'Name','Track Audio con Tono 7K','NumberTitle','off');
-stem(vector_tiempo, (vector_senial_monoL + seno7K), 'r','LineWidth',3);grid on;
+plot(vector_tiempo, (vector_senial_monoL + seno7K), 'r','LineWidth',1);grid on;
 
 title('Sinusoidal','FontSize',11,'FontName','Arial')
 xlabel('Tiempo [seg]','FontSize',11,'FontName','Arial')
 ylabel('Amplitud','FontSize',11,'FontName','Arial')
+
+%................. GRAFICACION senial con ruido filtrada .......................
+
+figure5 = figure('Color',[1 1 1],'Name','Track Audio filtrada','NumberTitle','off');
+plot(vector_tiempo, (vector_senial_sucia_filtrada), 'r','LineWidth',1);grid on;
+
+title('Sinusoidal','FontSize',11,'FontName','Arial')
+xlabel('Tiempo [seg]','FontSize',11,'FontName','Arial')
+ylabel('Amplitud','FontSize',11,'FontName','Arial')
+
+%%..................... GRAFICACION FFT Tono 7K ...........................
+figure6 = figure('Color',[1 1 1],'Name','Track Audio filtrada','NumberTitle','off');
+stem(freq, Modulo_filtrada, 'r','LineWidth',1);grid on;
+
+title('Espectro')
+xlabel('Frecuencia [Hz]','FontSize',11,'FontName','Arial')
+ylabel('Modulo1','FontSize',11,'FontName','Arial')
+
+
 
 
 
